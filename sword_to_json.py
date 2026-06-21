4903c4bb6f2907ac90ad9e9337ab95d633cc29d5
zamuwelle <xopxtech@gmail.com>
2026-06-21 19:23:17 +0800

    refactor: update to modern python, bible parsing

--- sword_to_json.py
+++ sword_to_json.py
@@ -1,56 +1,55 @@
 from pysword.modules import SwordModules
-import argparse, json, sys
-
-if sys.version_info > (3, 0):
-    from past.builtins import xrange
-
+import argparse, json
 
 def generate_dict(source_file, bible_version):
-    modules = SwordModules(source_file)
-    found_modules = modules.parse_modules()
-    bible = modules.get_bible_from_module(bible_version)
+	modules = SwordModules(source_file)
+	found_modules = modules.parse_modules()
 
-    books = bible.get_structure()._books['ot'] + bible.get_structure()._books['nt']
+	if bible_version not in found_modules:
+		raise SystemExit(
+			f"'{bible_version}' not found in {source_file}.\n"
+			f"Available module key(s): {list(found_modules.keys())}"
+		)
 
-    bib = {}
-    bib['books'] = []
+	bible = modules.get_bible_from_module(bible_version)
+	book_structure = bible.get_structure().get_books()
+	books = book_structure['ot'] + book_structure['nt']
 
-    for book in books:
-        chapters = []
-        for chapter in xrange(1, book.num_chapters+1):
-            verses = []
-            for verse in xrange(1, len(book.get_indicies(chapter))+1 ):
-                verses.append({
-                    'verse': verse,
-                    'chapter': chapter,
-                    'name': book.name + " " + str(chapter) + ":" + str(verse),
-                    'text': bible.get(books=[book.name], chapters=[chapter], verses=[verse])
-                    })
-            chapters.append({
-                'chapter': chapter,
-                'name': book.name + " " + str(chapter),
-                'verses': verses
-            })
-        bib['books'].append({
-            'name': book.name,
-            'chapters': chapters
-        })
-    
-    return bib
+	bib = {'books': []}
+	for book in books:
+		chapters = []
+		for chapter in range(1, book.num_chapters + 1):
+			texts = bible.get_iter(books=[book.name], chapters=[chapter])
+			verses = [
+				{
+					'verse': v,
+					'chapter': chapter,
+					'name': f"{book.name} {chapter}:{v}",
+					'text': t,
+				}
+				for v, t in enumerate(texts, start=1)
+			]
+			chapters.append({
+				'chapter': chapter,
+				'name': f"{book.name} {chapter}",
+				'verses': verses,
+			})
+		bib['books'].append({'name': book.name, 'chapters': chapters})
+
+	return bib
 
 def write_json(bible_dict, output_file):
-    with open(output_file, 'w') as outfile:  
-        json.dump(bible_dict, outfile)
-
+	with open(output_file, 'w') as outfile:
+		json.dump(bible_dict, outfile, indent='\t')
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--source_file')
-    parser.add_argument('--bible_version')
-    parser.add_argument('--output_file')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--source_file', required=True)
+	parser.add_argument('--bible_version', required=True)
+	parser.add_argument('--output_file', required=True)
+	args = parser.parse_args()
+	bible_dict = generate_dict(args.source_file, args.bible_version)
+	write_json(bible_dict, args.output_file)
 
-    bible_dict = generate_dict(args.source_file, args.bible_version)
-    write_json(bible_dict, args.output_file)
-
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+	main()
